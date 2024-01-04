eba4a5e84e329b9ac89d83b9785fc91e338ac19d
Rohit Tarang R <rohittarang512005@gmail.com>
2024-01-04 18:11:36 +0530

    files added

---
+++ server/database/db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+export const Connection = async (username,password) => {
+  const URL =
+    `mongodb+srv://${username}:${password}@ecommerce-web.hr9hnoi.mongodb.net/?retryWrites=true&w=majority`;
+
+  try {
+    await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true });
+    console.log("Database connected successfully")
+  } catch (error) {
+    console.log("Error while connection with the database ", error.message);
+  }
+}; 
+
+export default Connection;
